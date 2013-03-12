461705204b21a5a61533024e0e1c090d75939746
zyweistart <zyweistart@gmail.com>
2013-03-12 16:57:51 +0000

    sss

---
+++ iOS/Cysz/Cysz/STMainViewController.m
@@ -0,0 +1,26 @@
+//
+//  STMainViewController.m
+//  Cysz
+//
+//  Created by Start on 3/13/13.
+//  Copyright (c) 2013 Start. All rights reserved.
+//
+
+#import "STMainViewController.h"
+
+@interface STMainViewController ()
+
+@end
+
+@implementation STMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
