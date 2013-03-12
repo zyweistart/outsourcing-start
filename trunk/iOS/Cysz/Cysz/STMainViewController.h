fe5585250781f0e1fd74c0150bbe8f9730a8baf7
zyweistart@gmail.com <zyweistart@gmail.com@96581286-773c-d825-d549-28fffc3ece74>
2013-03-12 16:57:51 +0000

    sss

---
+++ trunk/iOS/Cysz/Cysz/STMainViewController.h
@@ -0,0 +1,14 @@
+//
+//  STMainViewController.h
+//  Cysz
+//
+//  Created by Start on 3/13/13.
+//  Copyright (c) 2013 Start. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseTableViewController.h"
+
+@interface STMainViewController : BaseTableViewController
+
+@end
